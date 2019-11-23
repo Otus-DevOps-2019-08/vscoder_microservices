f208607ba9cab89c5b753058bcdbfed1515daa29
Aleksey Koloskov <vsyscoder@gmail.com>
2019-11-23 19:57:32 +0300

    Fix travis tests passing

--- .travis-scripts/install.sh
+++ .travis-scripts/install.sh
@@ -8,5 +8,5 @@ make install_packer install_terraform install_tflint
 sudo apt-get update && sudo apt-get install python-virtualenv
 make install_requirements_virtualenv
 # Fix Failed to validate the SSL certificate for galaxy.ansible.com:443 #795 https://github.com/ansible/galaxy/issues/795
-"./.venv/bin/pip install -U urllib3[secure]"
+./.venv/bin/pip install -U "urllib3[secure]"
 make monolith_ansible_install_requirements
