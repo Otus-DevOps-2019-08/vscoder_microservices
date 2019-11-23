19f1d724dde9b239ace29a988d07a284221ef407
Aleksey Koloskov <vsyscoder@gmail.com>
2019-11-23 18:49:06 +0300

    Update apt cache before install packages

--- .travis-scripts/install.sh
+++ .travis-scripts/install.sh
@@ -5,7 +5,7 @@
 set -exu
 
 make install_packer install_terraform install_tflint
-sudo apt-get install python-virtualenv
+sudo apt-get update && sudo apt-get install python-virtualenv
 make install_requirements_virtualenv
 # Fix Failed to validate the SSL certificate for galaxy.ansible.com:443 #795 https://github.com/ansible/galaxy/issues/795
 "./.venv/bin/pip install -U urllib3[secure]"
