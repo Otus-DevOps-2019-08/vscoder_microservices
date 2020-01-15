9de632e452fc24698f6a4c097d6206a1586c54d8
Aleksey Koloskov <vsyscoder@gmail.com>
2020-01-16 00:21:21 +0300

    Update kubernetes/docker-helm

--- kubernetes/docker-helm/docker_build.sh
+++ kubernetes/docker-helm/docker_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eux
 
-echo docker build -t ${REGISTRY_USER_NAME}/${IMAGE}:v${HELM_VERSION} .
-docker build -t ${REGISTRY_USER_NAME}/${IMAGE}:v${HELM_VERSION} .
+echo docker build --build-arg HELM_VERSION=${HELM_VERSION} -t ${REGISTRY_USER_NAME}/${IMAGE}:v${HELM_VERSION} .
+docker build --build-arg HELM_VERSION=${HELM_VERSION} -t ${REGISTRY_USER_NAME}/${IMAGE}:v${HELM_VERSION} .
